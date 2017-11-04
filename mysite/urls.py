257e929fd4b6edda1ce3692d194442a83f615501
Dylan Staatz <dms9800@gmail.com>
2017-11-04 02:21:51 -0500

    Restructed Project and organized settings.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""syntacticsugar URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^history/', include('history.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', views.index),
 ]
