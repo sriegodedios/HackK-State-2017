442ff64fd437442cfec2e7648619cb34a132c0ca
Dylan Staatz <dms9800@gmail.com>
2017-11-04 15:48:04 -0500

    Organized templates

--- history/urls.py
+++ history/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.shortcuts import render
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'map_demo/', views.generic, {'template_name': 'history/google_map_demo.html'}, name='map_demo'),
 ]
--- history/views.py
+++ history/views.py
@@ -9,9 +9,12 @@ from .models import *
 def index(request):
     ''' Sends a list of the worst hurricanes overall
     '''
-    
+    header = ['Date', 'Name', 'Wind Speed', 'Category']
     output = []
-    for i in Hurricane.objects.order_by('max_wind')[:30]:
+    for i in Hurricane.objects.order_by('-max_wind')[:30]:
         output.append([i.start_date, i.name, i.max_wind, i.category])
 
-    return render(request, 'history/base.html', {'table': output})
+    return render(request, 'history/index.html', {'table_head': header, 'table': output})
+
+def generic(request, template_name):
+    return render(request, template_name)
