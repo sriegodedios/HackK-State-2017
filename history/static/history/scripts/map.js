11f5b8da9b25495a6edb1dc9d93ebce259f27fe0
Phillip Wendte <alexwendte@ksu.edu>
2017-11-05 03:45:30 -0600

    Map WORKING!!!! And Styling

--- history/static/history/scripts/map.js
+++ history/static/history/scripts/map.js
@@ -1,36 +1,48 @@
+ function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
 
-function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: -34.397, lng: 150.644}
-        });
-        var geocoder = new google.maps.Geocoder();
-
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-        alert("why");
-        document.getElementById('submit').addEventListener('keypress', function(event) {
-          event.preventDefault();
-          if(event.keyCode === 13){
-            geocodeAddress(geocoder, map);
-            //document.getElementById("id_of_button").click();
-          }
+    document.getElementById('submit').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
+    
+    
+    document.getElementById('address').addEventListener('keyup', function(event) {
+      event.preventDefault();
+      if(event.keyCode === 13){
+        geocodeAddress(geocoder, map);
+      }
+      });
+      
+      google.maps.event.addListener(map, 'click', function(event) {
+        loc = event.latLng;
+        placeMarker(loc);
+        window.location.href="/worst/" + loc.lat() + "/" + loc.lng();
+      });
+      
+      function placeMarker(location) {
+          var marker = new google.maps.Marker({
+              position: location, 
+              map: map
           });
       }
-
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            var loc = results[0].geometry.location;
-            resultsMap.setCenter();
-            var marker = new google.maps.Marker({map: resultsMap, position: loc});
-            alert(loc.lat());
-            window.location.href="/worst/" + loc.lat() + "/" + loc.lng();
-          } 
-          else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
+  }
+  
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        var loc = results[0].geometry.location;
+        resultsMap.setCenter();
+        var marker = new google.maps.Marker({map: resultsMap, position: loc});
+        alert(loc.lat());
+        window.location.href="/worst/" + loc.lat() + "/" + loc.lng();
+      } 
+      else {
+        alert('Geocode was not successful for the following reason: ' + status);
       }
+    });
+  }
